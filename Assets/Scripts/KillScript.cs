6cc509964d2875fe47d382596b21f14f1b35a4fb
s3000x <67296786+s3000x@users.noreply.github.com>
2020-07-25 09:35:46 -0400

    --- Auto-Git Commit ---

---
+++ Assets/Scripts/KillScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillScript : MonoBehaviour
+{
+    public float killTime;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        killTime -= Time.deltaTime;
+        if (killTime <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}

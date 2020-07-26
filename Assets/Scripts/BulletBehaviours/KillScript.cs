c517ae7d823fadf9fa4e84a3495063a182dd4d93
s3000x <67296786+s3000x@users.noreply.github.com>
2020-07-26 02:23:25 -0400

    --- Auto-Git Commit ---

--- Assets/Scripts/BulletBehaviours/KillScript.cs
+++ Assets/Scripts/BulletBehaviours/KillScript.cs
@@ -5,23 +5,48 @@ using UnityEngine;
 public class KillScript : MonoBehaviour
 {
     public float killTime;
+    public bool killModeDestroy;
+    public bool killModeDeactivate;
 
+    Collider2D killCollider;
+    SpriteRenderer killRender;
+    private float killReset;
 
-
+    private bool canKill;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
+       killCollider = GetComponent<Collider2D>();
+       killRender = GetComponent<SpriteRenderer>();
+       canKill = true;
+       killReset = killTime;
+       
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+    if (killModeDestroy == true)
     {
         killTime -= Time.deltaTime;
         if (killTime <= 0.0f)
         {
             Destroy(this.gameObject);
         }
-
+    }
+    else
+    {
+        killTime -= Time.deltaTime;
+        if (killTime <= 0.0f && canKill == true)
+        {
+            killCollider.enabled = !killCollider.enabled;
+            killRender.enabled = !killRender.enabled;
+            canKill = false;
+            killModeDestroy = true;
+            killTime += killReset + 3;
+        }
+    }
     }
 }

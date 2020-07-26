6ac95e40c39365b5abf9b49567f64f19814107d5
s3000x <67296786+s3000x@users.noreply.github.com>
2020-07-25 20:52:17 -0400

    --- Auto-Git Commit ---

--- Assets/Scripts/BulletBehaviours/Movement.cs
+++ Assets/Scripts/BulletBehaviours/Movement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Vector2 emitterPos;
+    private Vector2 emitterPosB;
+
+    private Vector2 emitterMove;
+
+    private float speed;
+
+    Rigidbody2D emitterBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        emitterBody = GetComponent<Rigidbody2D>();
+        emitterPos = emitterBody.transform.position;
+        emitterPosB = new Vector2(emitterPos.x + 7, emitterPos.y);
+        speed = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.Lerp(emitterPos, emitterPosB, Mathf.PingPong(speed * Time.deltaTime, 10));
     }
 }

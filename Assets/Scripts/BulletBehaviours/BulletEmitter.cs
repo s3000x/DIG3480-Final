ddae86391782e8aca45393d5fd438f0556809312
s3000x <67296786+s3000x@users.noreply.github.com>
2020-07-25 23:22:51 -0400

    --- Auto-Git Commit ---

---
+++ Assets/Scripts/BulletBehaviours/BulletEmitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEmitter : MonoBehaviour
+{
+    public GameObject objForEmit;
+    public float time;
+    private float originalTime;
+
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalTime = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            GameObject emitObj = (GameObject)Instantiate(objForEmit, rigidbody2D.position, Quaternion.identity);
+            time = originalTime; 
+        }
+    }
+}

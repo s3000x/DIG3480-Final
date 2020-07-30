d90b0a25cfe230832b2d2b0f3e45534dbc825a70
s3000x <67296786+s3000x@users.noreply.github.com>
2020-07-30 14:10:03 -0400

    --- Auto-Git Commit ---

---
+++ Assets/Scripts/AsteroidMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovement : MonoBehaviour
+{
+    private float scale;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate (new Vector3 (0, 0, 15) * Time.deltaTime);
+       transform.localScale += new Vector3 (1, 1, 1) * Time.deltaTime;
+
+    }
+}

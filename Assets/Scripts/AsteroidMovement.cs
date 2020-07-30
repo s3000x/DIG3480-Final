a22e93472f9fc4ad9b0e89552c6a07aabdb3209f
s3000x <67296786+s3000x@users.noreply.github.com>
2020-07-30 14:34:29 -0400

    added asteroid. Making final tweaks before publishing

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class AsteroidMovement : MonoBehaviour
 {
-    private float scale;
-    
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log ("" + changeTime);
        transform.Rotate (new Vector3 (0, 0, 15) * Time.deltaTime);
-       transform.localScale += new Vector3 (1, 1, 1) * Time.deltaTime;
-
     }
 }
